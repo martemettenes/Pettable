b9b94b9ceabc2c976d298c6eb2a3a236a7721f51
Marte <marte.mettenes@noaignite.com>
2023-04-28 10:19:57 +0200

    update +page with hero component with photo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import Container from '$lib/components/Container.svelte'
+    import Hero from '$lib/components/Hero.svelte'
 </script>
 
-
-<h1> Welcome to Pettable </h1>
-<Container />
+<!-- <Container>
+    <h1> Pettable pet care system </h1>
+</Container> -->
+<Hero imgSrc='src/lib/assets/img/dog-img.jpeg' imgAlt='Photo of a vizsla, a dog with short red fur' title='Pettable pet care system' heroButtonText='Get started' />
