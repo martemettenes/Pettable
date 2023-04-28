9d5167115f35ee2bd2f1d13becec21f23e63a6d5
Marte <marte.mettenes@noaignite.com>
2023-04-28 10:17:39 +0200

    add logo and styling to header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,23 +1,29 @@
 <script>
     import Nav from '$lib/components/Nav.svelte'
+
+    let src = 'src/lib/assets/logo/Pettable.svg';
 </script>
 
 <header>
-    <a href="/"> Home </a>
-    <Nav />
+    <section>
+        <a href="/">
+            <img {src} alt="Pettable logo">  
+        </a>
+        <Nav />
+    </section>
 </header>
 
-<style>
+<style lang="scss">
     header {
         padding: 1rem;
         background: #1E442E;
         color: #A0CBB1;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
 
-    }
-        a {
-            color: #A0CBB1;
+        section {
+            margin: 0 auto;
+            display: flex;
+            justify-content: space-between;
+            max-width: 2000px;
         }
+    }
 </style>
