9a2a97dc271affd85e89002ef54b38fae7c2eb44
Marte <marte.mettenes@noaignite.com>
2023-05-30 13:00:45 +0200

    add color style to button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,20 +1,38 @@
 <script>
     export let buttonText = 'buttonText'
+    export let style = 'light'
 </script>
 
-<button>
+{#if style === 'light'}
+<button class="button--light-purple">
     {buttonText}
 </button>
+{:else if style === 'dark'}
+<button class="button--dark-green">
+    {buttonText}
+</button>
+{:else} 
+<button class="button--light-purple">
+    {buttonText}
+</button>
+{/if}
 
-<style lang="scss">
+<style lang='scss'>
     button {
-        background: #457659;
-        color: #fff;
-        padding: 4px 16px;
+        padding: 8px 16px;
         font-size: 18px;
         line-height: 22px;
         border: none;
         border-radius: 50px;
     }
 
+    .button--dark-green {
+        background: #457659;
+        color: white;
+    }
+
+    .button--light-purple {
+        background: #F0EBFF;
+        color: #301E63;
+    }
 </style>
