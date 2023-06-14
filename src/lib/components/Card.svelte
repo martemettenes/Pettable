00f06e6ed3c5c60888d57ac8fb02cc9fd18a8560
Marte <marte.mettenes@noaignite.com>
2023-06-14 13:19:43 +0200

    create card component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,45 @@
+<script>
+import { urlFor } from '$lib/utils/image.js'
+import '$lib/styles/style.scss';
+
+export let imgSrc = 'imgSrc';
+export let imgAlt = 'imgAlt';
+</script>
+
+
+<div class="card">
+    <div class="cardImgContainer ratio ratio--1-1">
+        <img class="ratio__item ratio__item--cover" src={urlFor(imgSrc).url()} alt={imgAlt}/>
+    </div>
+    <div class="cardTextContainer">
+        <slot />
+    </div>
+</div>
+
+<style lang="scss">
+    .card {
+        color: #EBFFF3;
+        grid-column: span 4;
+
+        @media screen and (min-width: 480px) {
+            grid-column: span 2;
+        }
+
+        @media screen and (min-width: 768px) {
+            grid-column: span 4;
+        }
+
+        .cardImgContainer {
+            width: 100%;
+            overflow: hidden;
+            margin: 0;
+        }
+
+        .cardTextContainer {
+            padding: 1rem;
+            background-color: $color-purple-30;
+        }
+
+    }
+
+    </style>
