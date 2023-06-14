265f00b21e4b6dc1ce425f90917b634c5286f6dc
Marte <marte.mettenes@noaignite.com>
2023-06-14 13:19:51 +0200

    create cardgroup component

---
+++ src/lib/components/CardGroup.svelte
@@ -0,0 +1,22 @@
+<script>
+    import '$lib/styles/style.scss';
+</script>
+
+<div>
+    <slot />
+</div>
+
+<style lang="scss">
+    div {
+        //padding: 1rem;
+        display: grid;
+        grid-template-columns: repeat(4, minmax(0, 1fr));
+        column-gap: 1rem;
+        row-gap: 1rem;
+
+        @media screen and (min-width: 768px) {
+            grid-template-columns: repeat(12, minmax(0, 1fr));
+        }
+
+    }
+</style>
